efadd5c47ceb27c8a6e081f6e70c4d1b84f2ab44
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2020-03-02 14:58:45 +0400

    add ok module

---
+++ source/modules/ok.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Odnoklassniki.ru
+"""
+from html2text import convert
+from . import by_subj, BUTTONS, NBSP
+
+LABEL = 'ok'
+
+SUBJ_POST = 'Пока вас не было'
+SUBJ_PRESENT = 'вам подарок'
+
+
+def e_message(_subj, text):
+    """
+    message
+    """
+    return [convert(text).replace(NBSP, ' ')]
+
+
+def e_present(subj, text):
+    """
+    present
+    """
+    link = ""
+    lines = iter(text.splitlines())
+    for line in lines:
+        if ('https://ok.ru/?' in line) and ('lookPresent' in line):
+            link = "[Посмотреть]({})".format(line)
+            break
+
+    return [subj, '', BUTTONS, link]
+
+
+SUBJ_HANDLERS = [
+  ((SUBJ_POST, ), e_message),
+  ((SUBJ_PRESENT, ), e_present),
+]
+
+
+def start(subj, body):
+    """
+    parse FaceBook
+    """
+    return by_subj(subj, body, body, LABEL, 'Одноклассники: ', SUBJ_HANDLERS)
---
+++ tests/test/test_ok.py
@@ -0,0 +1,35 @@
+"""
+make test T=test_ok.py
+"""
+from . import TestCase
+
+
+class TestOk(TestCase):
+    """
+    Odnoklassniki.ru
+    """
+    mark = "<html>"
+
+    def transfer(self, fname):
+        """
+        transfer fixture by ok
+        """
+        from modules.ok import start
+        return self.start_transfer(fname, start, 'ok')
+
+    def test_message(self):
+        """
+        message
+        """
+        text = self.transfer('message.txt')
+        assert self.mark not in text
+
+    def test_present(self):
+        """
+        present
+        """
+        text = self.transfer('present.txt')
+        assert self.mark not in text
+
+        text = self.transfer('present1.txt')
+        assert self.mark not in text
