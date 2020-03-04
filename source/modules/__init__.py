4e6c09550e8d26560b15871179836186e1b94e35
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2020-03-04 11:38:22 +0400

    Refactor (#19)

--- source/modules/__init__.py
+++ source/modules/__init__.py
@@ -17,12 +17,19 @@ def store(label, subj, body):
     return subj + '\n' + convert(body, extract_link=True).replace(NBSP, ' ')
 
 
+def is_present(marks, text):
+    """
+    return True, if all marks present in given text
+    """
+    return all([mark in text for mark in marks])
+
+
 def by_subj(subj, body, text, label, prefix, handlers):  # pylint: disable=too-many-arguments
     """
     process message by subject
     """
     for marks, func in handlers:
-        if all([mark in subj for mark in marks]):
+        if is_present(marks, subj):
             return prefix + '\n'.join(func(subj, text))
 
     # unknown subject, save to db for analizing and return default answer
