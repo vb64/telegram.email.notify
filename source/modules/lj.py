ab1c8117541e8c13919db8b9b8d5d0f2f51c8e70
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2020-03-10 10:55:19 +0400

    lj parse

--- source/modules/lj.py
+++ source/modules/lj.py
@@ -1,14 +1,26 @@
+# -*- coding: utf-8 -*-
 """
 LiveJournal
 """
 from models import SavedSource
+from . import convert, NBSP, BUTTONS
+
 LABEL = 'lj'
+MARK_END = 'https://'
 
 
 def start(subj, body):
     """
     parse LiveJournal message
     """
-    SavedSource(label=LABEL, subject=subj, body=body).put()
+    text = convert(body, extract_link=True).replace(NBSP, ' ')
+    link = ''
+    if MARK_END in text:
+        pos = text.index(MARK_END)
+        title = text[:pos]
+        link = '\n' + BUTTONS + '\n' + "[Читать]({})".format(text[pos:].split()[0])
+        text = title
+    else:
+        SavedSource(label=LABEL, subject=subj, body=body).put()
 
-    return 'LiveJournal: ' + subj
+    return 'LiveJournal: ' + subj + '\n' + text + link
