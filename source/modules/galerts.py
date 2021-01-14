0ea7319149ea1bd8cb8f9c364a28573452d95665
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-01-14 15:50:58 +0400

    refactor

--- source/modules/galerts.py
+++ source/modules/galerts.py
@@ -7,7 +7,6 @@ import urlparse
 from html2text2 import convert, Parser as BaseParser
 from . import by_subj, NBSP
 
-MARK_START_HTML = '<div dir="ltr">'
 MARK_SKIP = u"Ещё результаты"
 MARK_NORELEVANT = u"Пометить как нерелевантный"
 MARK_NEWS = u"НОВОСТИ"
@@ -27,20 +26,6 @@ class Parser(BaseParser):
         return text
 
 
-def alert_ru(_subj, text):
-    """
-    alert with ru language
-    """
-    return [
-      text,
-    ]
-
-
-SUBJ_HANDLERS = [
-  (('Оповещение Google ', ), alert_ru),
-]
-
-
 def drop_lines(text, marks):
     """
     drop lines starting with any substring from marks
@@ -53,19 +38,33 @@ def drop_lines(text, marks):
     return u'\n'.join(ret)
 
 
+def alert_ru(subj, text):
+    """
+    alert with ru language
+    """
+    pos_skip = text.index(MARK_SKIP)
+    if pos_skip >= 0:
+        text = text[:pos_skip]
+
+    return [
+      subj.decode('utf-8'),
+      '',
+      Parser.drop_newlines(drop_lines(text, [MARK_NORELEVANT, MARK_NEWS])),
+    ]
+
+
+SUBJ_HANDLERS = [
+  (('Оповещение Google ', ), alert_ru),
+]
+
+
 def start(subj, body):
     """
     parse Google Alerts
     """
     text = body
-    pos_start = text.index(MARK_START_HTML)
+    pos_start = text.index('<div dir="ltr">')
     if pos_start >= 0:
         text = convert(Parser, body[pos_start:].replace(NBSP, ' '), extract_link=True)
 
-    text = by_subj(subj, body, text, 'galerts', u'Оповещение Google\n\n', SUBJ_HANDLERS)
-
-    pos_skip = text.index(MARK_SKIP)
-    if pos_skip >= 0:
-        text = text[:pos_skip]
-
-    return Parser.drop_newlines(drop_lines(text, [MARK_NORELEVANT, MARK_NEWS]))
+    return by_subj(subj, body, text, 'galerts', '', SUBJ_HANDLERS)
