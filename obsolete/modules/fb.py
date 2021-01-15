70d33ac61d8d2d5db9e6ebf811f99e463cbd36b4
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-01-15 15:00:55 +0400

    fix Facebook notifications (#46)

---
+++ obsolete/modules/fb.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+FaceBook
+"""
+from models import SavedSource
+from . import by_subj, BUTTONS
+
+LABEL = 'fb'
+MARK_VIEW = 'Посмотреть на Facebook'
+MARK_ACCEPT = 'Подтвердить запрос'
+
+SUBJ_COMMENT = 'Посмотрите комментарий'
+SUBJ_POST = 'Посмотрите новую публикацию'
+SUBJ_FRIEND = 'хочет стать вашим другом на Facebook'
+SUBJ_FRIEND1 = 'запрос на добавление в друзья'
+SUBJ_PHOTO = 'Посмотрите новое фото'
+
+
+def read_citate(lines):
+    """
+    read citate from iterator
+    """
+    ret = []
+    for line in lines:
+        next_line = None
+        if line == 'С уважением,':
+            next_line = next(lines)
+            if next_line == 'Команда Facebook':
+                break
+
+        ret.append(line.strip('"'))
+        if next_line is not None:
+            ret.append(next_line.strip('"'))
+
+    return '\n'.join(ret)
+
+
+def get_handler(prefix, mark):
+    """
+    closure for subj handler
+    """
+    def e_post(subj, text):
+        """
+        post
+        """
+        link = ''
+        title = ''
+        citate = ''
+
+        lines = iter(text.splitlines())
+        for line in lines:
+            if line.startswith(mark):
+                link = "[{}]({})".format(mark, next(lines))
+            elif line.startswith(prefix):
+                title = line
+            elif line in ['Посетить группу', 'Читать публикацию']:
+                citate = read_citate(lines)
+                break
+
+        if not all([link, title]):
+            SavedSource(label=LABEL, subject=subj, body=text).put()
+
+        return [title, '', citate, BUTTONS, link]
+
+    return e_post
+
+
+def e_subj(subj, _text):
+    """
+    dubj only
+    photo, recommendation, update
+    """
+    return [subj]
+
+
+SUBJ_HANDLERS = [
+  ((SUBJ_COMMENT, ), get_handler(SUBJ_COMMENT, MARK_VIEW)),
+  ((SUBJ_POST, ), get_handler(SUBJ_POST, MARK_VIEW)),
+  ((SUBJ_FRIEND, ), get_handler(SUBJ_FRIEND, MARK_ACCEPT)),
+  ((SUBJ_FRIEND1, ), get_handler(SUBJ_FRIEND1, MARK_ACCEPT)),
+  ((SUBJ_PHOTO, ), get_handler(SUBJ_PHOTO, MARK_VIEW)),
+  (('опубликовал обновление', ), e_subj),
+  (('добавил', ' новое фото'), e_subj),
+  (('У вас ', ' новых рекомендаций'), e_subj),
+  (('У вас ', ' новые рекомендации'), e_subj),
+  (('новые рекомендации групп:', ), e_subj),
+]
+
+
+def start(subj, body):
+    """
+    parse FaceBook
+    """
+    return by_subj(subj, body, body, LABEL, 'FaceBook: ', SUBJ_HANDLERS)
---
+++ obsolete/tests/test_fb.py
@@ -0,0 +1,74 @@
+"""
+make test T=test_fb.py
+"""
+from . import TestCase
+
+
+class TestFB(TestCase):
+    """
+    FaceBook
+    """
+    mark = "https://www.facebook.com/email_forward_notice"
+
+    def transfer(self, fname):
+        """
+        transfer fixture by facebook
+        """
+        from modules.fb import start
+        return self.start_transfer(fname, start, 'fb')
+
+    def test_comment(self):
+        """
+        comment
+        """
+        text = self.transfer('comment.txt')
+        assert self.mark not in text
+
+    def test_broken_comment(self):
+        """
+        broken comment
+        """
+        text = self.transfer('comment1.txt')
+        assert self.mark not in text
+
+        text = self.transfer('comment2.txt')
+        assert self.mark in text
+
+    def test_photo(self):
+        """
+        photo
+        """
+        text = self.transfer('photo.txt')
+        assert self.mark not in text
+
+        text = self.transfer('photo1.txt')
+        assert self.mark not in text
+
+        text = self.transfer('update.txt')
+        assert self.mark not in text
+
+    def test_recomendation(self):
+        """
+        recomendation
+        """
+        text = self.transfer('recomendation.txt')
+        assert self.mark not in text
+
+        text = self.transfer('recomendation1.txt')
+        assert self.mark not in text
+
+        text = self.transfer('recomendation2.txt')
+        assert self.mark not in text
+
+    def test_friend(self):
+        """
+        friend
+        """
+        text = self.transfer('friend.txt')
+        assert self.mark not in text
+
+        text = self.transfer('friend1.txt')
+        assert self.mark not in text
+
+        text = self.transfer('friend2.txt')
+        assert self.mark not in text
