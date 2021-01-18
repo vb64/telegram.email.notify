38b933858029f79025649ce484b8b8b4240a30a1
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-01-18 11:06:34 +0400

    step

--- source/modules/reddit.py
+++ source/modules/reddit.py
@@ -3,12 +3,71 @@
 Reddit
 """
 from html2text2 import Parser
-from . import is_present
-
-MARK_POSTED = 'Posted by'
+from . import is_present, make_markdown
 
 
-def start(subj, body):
+class Section:
+    """
+    Reddit message section
+    """
+    def __init__(self):
+        self.lines = []
+        self.read_more = None
+        self.comments = None
+        self.votes = None
+
+    def __unicode__(self):
+        return u"{}\n\n{} {} {}".format(
+          '\n\n'.join(self.lines),
+          self.read_more,
+          self.comments,
+          self.votes,
+        )
+
+    def __str__(self):
+        return self.__unicode__().encode('utf-8')
+
+    def add_line(self, text):
+        """
+        return true if detected end if section
+        """
+        if text.endswith(' Votes '):
+            return self.add_votes(text)
+        elif text.endswith(' Comments '):
+            return self.add_comments(text)
+        elif text.startswith('Read ') and text.endswith(' More '):
+            return self.add_readmore(text)
+        else:
+            if text.strip():
+                self.lines.append(make_markdown(text.decode('utf-8')))
+            return False
+
+    def add_readmore(self, text):
+        """
+        line with read more link
+        """
+        words = text.split()
+        self.read_more = "Read [more]({})".format(words[1])
+        return False
+
+    def add_comments(self, text):
+        """
+        line with comments
+        """
+        words = text.split()
+        self.comments = "comments [{}]({})".format(words[0], words[1])
+        return False
+
+    def add_votes(self, text):
+        """
+        line with votes
+        """
+        words = text.split()
+        self.votes = "votes [{}]({})".format(words[0], words[1])
+        return True
+
+
+def start(_subj, body):
     """
     parse Reddit message
     """
@@ -16,11 +75,14 @@ def start(subj, body):
     parser.feed(body)
     parser.close()
     text = parser.text()
-    lines = []
+    sections = []
+    section = Section()
 
     for line in text.split('\n')[2:]:
         if is_present(('VIEW MORE', 'POSTS '), line):
             break
-        lines.append(line)
+        if section.add_line(line):
+            sections.append(str(section))
+            section = Section()
 
-    return 'Reddit: ' + subj + '\n\n' + '\n'.join(lines)
+    return 'Reddit news' + '\n\n' + '\n\n'.join(sections)
