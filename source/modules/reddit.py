638f5abab70df8617673ca96d74cfe0ceeea22e5
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-01-19 11:14:08 +0400

    Fix reddit (#49)

--- source/modules/reddit.py
+++ source/modules/reddit.py
@@ -1,33 +1,90 @@
 """
 Reddit
 """
-from models import SavedSource
-from . import convert, NBSP, BUTTONS
-
-LABEL = 'reddit'
-MARK_CLICK = 'https://click.redditmail.com/'
-MARK_POSTED = 'Posted by'
+from html2text2 import Parser
+from . import is_href, is_present, MARKUP
 
 
-def start(subj, body):
+def clear_links(text):
+    """
+    Clear links from text
+    """
+    words = []
+    for i in text.split():
+        if not is_href(i):
+            words.append(i)
+
+    return ' '.join(words)
+
+
+class Section:
+    """
+    Reddit message section
+    """
+    def __init__(self, text):
+        self.skiplines = [
+          ' Votes ',
+          ' Comments ',
+        ]
+        self.lines = [clear_links(text)]
+        self.read_more = None
+
+    def __unicode__(self):
+        return "{}\n\n{}".format(
+          '\n\n'.join(self.lines),
+          self.read_more,
+        )
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def add_line(self, text):
+        """
+        return true if detected end if section
+        """
+        for i in self.skiplines:
+            if text.endswith(i):
+                return
+
+        if text.startswith('Read ') and text.endswith(' More '):
+            self.add_readmore(text)
+        else:
+            if text.strip():
+                self.lines.append(clear_links(text))
+
+    def add_readmore(self, text):
+        """
+        line with read more link
+        """
+        words = text.split()
+        self.read_more = "[Read more]({})".format(words[1])
+        return False
+
+
+def start(_subj, body):
     """
     parse Reddit message
     """
-    text = convert(body, extract_link=True).replace(NBSP, ' ')
+    parser = Parser(True, True)
+    parser.feed(body)
+    parser.close()
+    text = parser.text()
+    sections = []
+    section = None
 
-    if MARK_CLICK not in text:
-        SavedSource(label=LABEL, subject=subj, body=body).put()
-        return 'Reddit: ' + subj + '\n' + text
+    for line in text.split('\n')[2:]:
+        if 'Posted by ' in line:
+            if section:
+                sections.append(str(section))
+            section = Section(line)
+            continue
 
-    border = text.index(MARK_CLICK)
-    head = text[:border]
-    tail = text[border:]
+        if is_present(('VIEW MORE', 'POSTS '), line):
+            break
 
-    if MARK_POSTED in head:
-        head = ' '.join(head[head.index(MARK_POSTED) + len(MARK_POSTED):].strip().split()[1:])
-    link = "[Read]({})".format(tail.split()[0])
+        if section:
+            section.add_line(line)
 
-    if not head:
-        head = subj
+    sections.append(str(section))
 
-    return 'Reddit: ' + head + '\n' + BUTTONS + '\n' + link
+    return MARKUP + '\n' 'Reddit news' + '\n\n' + '\n\n'.join(sections)
