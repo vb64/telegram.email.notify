9042279b57debf083866da1d211c44482cd81461
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-01-20 17:07:04 +0400

    test upload

--- tests/test/test_views.py
+++ tests/test/test_views.py
@@ -86,3 +86,36 @@ class TestCaseViews(TestCase):
         response = self.param_post('transform', {'codec': 'store'}, 'testdata')
         assert response.status_code == 200
         assert response.data == 'OK'
+
+    def test_email(self):
+        """
+        email page
+        """
+        response = self.simple_view('email_upload')
+        assert response.status_code == 200
+        assert '/upload/' in response.data
+
+    @staticmethod
+    def test_email_upload():
+        """
+        email upload page
+        """
+        from google.appengine.ext import blobstore
+        from google.appengine.api import datastore
+
+        # https://stackoverflow.com/questions/28851686/unit-testing-blobstore-create-upload-url-uploads
+        url = blobstore.create_upload_url('/upload/').split('/')[-1]
+        assert url
+        session = datastore.Get(url)
+        assert session  # !!!
+
+        # https://stackoverflow.com/questions/13304988/gae-how-to-use-blobstore-stub-in-testbed
+        # response = self.client.post(
+        #   url,
+        #   data={
+        #     'emailbody': [open(self.get_fixture_path('reddit01.eml'), 'rb')],
+        #   },
+        #   follow_redirects=True,
+        #   content_type='multipart/form-data'
+        # )
+        # assert response.status_code == 200
