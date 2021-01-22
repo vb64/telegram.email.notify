903e701706c585ab789fdc69a1cdc39170566f53
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-01-22 15:20:51 +0400

    coverage

--- tests/test/test_views.py
+++ tests/test/test_views.py
@@ -1,6 +1,7 @@
 """
 make test T=test_views.py
 """
+from flask import escape
 from . import TestCase
 
 
@@ -94,3 +95,25 @@ class TestCaseViews(TestCase):
         response = self.simple_view('email_upload')
         assert response.status_code == 200
         assert '/upload/' in response.data
+
+        response = self.guest_view('/email/666/', return_code=404)
+        assert response.status_code == 404
+
+        from models import EmailData
+
+        item = EmailData()
+        item.field_from = "xxxx"
+        item.field_to = "zzz"
+        item.field_subj = "yyy"
+        item.field_text = "123 456"
+        item.field_html = "<html></html>"
+        item.put()
+
+        response = self.guest_view('/email/{}/'.format(item.key.id()))
+        assert response.status_code == 200
+
+        assert item.field_from in response.data
+        assert item.field_to in response.data
+        assert item.field_subj in response.data
+        assert item.field_text in response.data
+        assert escape(item.field_html) in response.data
