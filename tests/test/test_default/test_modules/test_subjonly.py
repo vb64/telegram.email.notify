2e0be13c05d51a9b244c8019077923126d31465b
vb64 <vit.sar68@gmail.com>
2023-07-28 09:38:23 +0400

    step

--- tests/test/test_default/test_modules/test_subjonly.py
+++ tests/test/test_default/test_modules/test_subjonly.py
@@ -8,6 +8,11 @@ from . import TestModule
 class TestSubjonly(TestModule):
     """Subject only."""
 
+    def from_eml(self, fname):
+        """Fixture from eml file subjonly."""
+        from modules.subjonly import start
+        return self.start_eml(fname, start, 'subjonly')
+
     def transfer(self, fname):
         """Transfer fixture by beeline."""
         from modules.subjonly import start
@@ -18,3 +23,9 @@ class TestSubjonly(TestModule):
         text = self.transfer('voice_mail.txt')
         assert 'Облачная АТС - У вас новое сообщение голосовой почты' in text
         assert ' 9033756597' not in text
+
+    def test_eml(self):
+        """Check eml file."""
+        text = self.from_eml('msg01.eml')
+        assert 'Только тема' in text
+        assert 'тело письма' not in text
